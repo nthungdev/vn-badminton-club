c489bfef713d13994023a524e992b32df2f08c74
Hung Nguyen <nthungdev@gmail.com>
2024-10-07 19:20:11 -0400

    only show create event button for user

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
-import BasePage from "@/components/BasePage";
-import Link from "next/link";
+import { getMe } from '@/actions/auth'
+import BasePage from '@/components/BasePage'
+import { getUserRole } from '@/lib/authUtils'
+import { Role } from '@/lib/firebase/definitions'
+import Link from 'next/link'
 
-export default function PageHome() {
-  return <BasePage>
-    <h1 className="sr-only">Home</h1>
+export default async function PageHome() {
+  const me = await getMe()
 
-    <div className="flex flex-col items-center">
-    <Link
-      href="/event/create"
-      className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-primary text-white hover:bg-primary-700 focus:outline-none focus:bg-primary-700 disabled:opacity-50 disabled:pointer-events-none"
-    >
-      Create Event
-    </Link>
-    </div>
-  </BasePage>
+  const isAuth = !!me
+
+  return (
+    <BasePage>
+      <h1 className="sr-only">Home</h1>
+
+      <div className="flex flex-col items-center">
+        {isAuth && (
+          <Link
+            href="/event/create"
+            className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-primary text-white hover:bg-primary-700 focus:outline-none focus:bg-primary-700 disabled:opacity-50 disabled:pointer-events-none"
+          >
+            Create Event
+          </Link>
+        )}
+      </div>
+    </BasePage>
+  )
 }
