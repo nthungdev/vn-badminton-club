870ff7f3be5657ab96352d0ebf3ce1fe435f0ed3
Hung Nguyen <nthungdev@gmail.com>
2024-10-07 19:11:24 -0400

    hidden menu

--- lib/menu.ts
+++ lib/menu.ts
@@ -8,6 +8,7 @@ interface MenuItem {
   label: string
   href: string
   guard: AuthGuard
+  hideFromMenu: boolean
 }
 
 const menuHref = {
@@ -15,6 +16,7 @@ const menuHref = {
   signIn: '/signIn',
   signUp: '/signUp',
   event: '/event',
+  createEvent: '/event/create',
 }
 
 const menu: MenuItem[] = [
@@ -22,17 +24,26 @@ const menu: MenuItem[] = [
     label: 'Home',
     href: menuHref.home,
     guard: AuthGuard.Public,
+    hideFromMenu: false,
   },
   {
     label: 'Sign In',
     href: menuHref.signIn,
     guard: AuthGuard.UnauthenticatedRequired,
+    hideFromMenu: false,
   },
   {
     label: 'Sign Up',
     href: menuHref.signUp,
     guard: AuthGuard.UnauthenticatedRequired,
+    hideFromMenu: false,
   },
+  {
+    label: 'Create Event',
+    href: menuHref.createEvent,
+    guard: AuthGuard.AuthenticatedRequired,
+    hideFromMenu: true,
+  }
 ]
 
 export { menu, menuHref, AuthGuard }
