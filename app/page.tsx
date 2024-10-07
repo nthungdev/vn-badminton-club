97f53ca333b3157b3923ce2c3a68282c91693903
Hung Nguyen <nthungdev@gmail.com>
2024-10-07 17:19:58 -0400

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
+
 export default function PageHome() {
-  return <div>
-    Home
+  return <div className="py-4">
+    <h1 className="sr-only">Home</h1>
+
+    <div className="flex flex-col items-center">
+    <Link
+      href="/event/create"
+      className="py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-primary text-white hover:bg-primary-700 focus:outline-none focus:bg-primary-700 disabled:opacity-50 disabled:pointer-events-none"
+    >
+      Create Event
+    </Link>
+    </div>
   </div>
 }
