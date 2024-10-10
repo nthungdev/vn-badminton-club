f93c6bde20e5114c6bc424c8b34f768ecaa54368
Hung Nguyen <nthungdev@gmail.com>
2024-10-10 18:18:20 -0400

    update Join Event style

--- components/ParticipateEventButton.tsx
+++ components/ParticipateEventButton.tsx
@@ -16,7 +16,7 @@ export function ParticipateEventButton(props: ParticipateEventButtonProps) {
     <button
       type="button"
       className={classNames(
-        "w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent text-white focus:outline-none  disabled:opacity-50 disabled:pointer-events-none",
+        "mx-auto max-w-md w-full py-3 px-4 flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent text-white focus:outline-none disabled:opacity-50 disabled:pointer-events-none",
         props.joined ? 'bg-red-600 hover:bg-red-700 focus:bg-red-700' : 'bg-primary hover:bg-primary-700 focus:bg-primary-700'
       )}
       disabled={props.disabled}
