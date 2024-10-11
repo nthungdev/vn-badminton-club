c83bf898c4931ac0c43a5a5ea888c4bc56c76219
Hung Nguyen <nthungdev@gmail.com>
2024-10-11 05:04:34 -0400

    add update event route

--- app/api/event/route.ts
+++ app/api/event/route.ts
@@ -1,11 +1,73 @@
 import { createErrorResponse } from '@/lib/apiResponse'
-import { deleteEvent, getEventById } from '@/lib/firebase/firestore'
+import {
+  deleteEvent,
+  getEventById,
+  updateEvent,
+} from '@/lib/firebase/firestore'
 import { verifySession } from '@/lib/session'
 import { isRoleMod } from '@/lib/utils/auth'
 import { NextRequest } from 'next/server'
 
 interface EventDeleteRequest {
   eventId?: string
+}
+
+interface EventUpdateRequest {
+  eventId?: string
+  title?: string
+  startTimestamp?: number
+  endTimestamp?: number
+  slots?: number
+}
+
+export async function PATCH(request: NextRequest) {
+  const { decodedIdToken } = await verifySession()
+  if (!decodedIdToken) {
+    return createErrorResponse('Unauthorized', 401)
+  }
+
+  const data: EventUpdateRequest = await request.json()
+
+  if (!data.eventId) {
+    return createErrorResponse('Missing eventId', 400)
+  }
+
+  if (
+    !data.title ||
+    !data.startTimestamp ||
+    !data.endTimestamp ||
+    !data.slots
+  ) {
+    return createErrorResponse('Missing required fields', 400)
+  }
+
+  try {
+    const event = await getEventById(data.eventId)
+    if (!event) {
+      return createErrorResponse('Event not found', 404)
+    }
+    if (
+      event.organizer.uid !== decodedIdToken.uid &&
+      !(await isRoleMod(decodedIdToken.uid))
+    ) {
+      return createErrorResponse('Unauthorized', 401)
+    }
+
+    const updatedEvent = {
+      title: data.title,
+      startTimestamp: new Date(data.startTimestamp),
+      endTimestamp: new Date(data.endTimestamp),
+      slots: data.slots,
+    }
+
+    await updateEvent(data.eventId, updatedEvent)
+
+    console.log('Event updated:', data.eventId)
+    return Response.json({ success: true })
+  } catch (error) {
+    console.error('Error updating event:', error)
+    return createErrorResponse(error, 500)
+  }
 }
 
 export async function DELETE(request: NextRequest) {
