830907772ba410b8177af18ba8525df315d8a70d
Hung Nguyen <nthungdev@gmail.com>
2024-10-13 03:34:36 -0400

    update client firebase

--- lib/firebase/clientApp.ts
+++ lib/firebase/clientApp.ts
@@ -2,12 +2,11 @@
 
 // Add SDKs for Firebase products to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { initializeApp } from 'firebase/app'
-import { getAuth } from "firebase/auth";
+import { getApp, getApps, initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 import { firebaseConfig } from './config'
 
-
-const firebaseApp = initializeApp(firebaseConfig)
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 const auth = getAuth(firebaseApp)
 
 export { firebaseApp, auth }
