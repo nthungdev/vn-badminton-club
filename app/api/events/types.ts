c22a5946c47fb8e0cca55b252a9f4ed3c9d85655
Hung Nguyen <nthungdev@gmail.com>
2024-10-20 04:42:09 -0400

    update response type

--- app/api/events/types.ts
+++ app/api/events/types.ts
@@ -2,3 +2,7 @@ import { ApiResponse } from "@/lib/apiResponse";
 import { HomeViewEvent } from "@/lib/firebase/definitions/event";
 
 export type EventsGetResponse = ApiResponse<{ events: HomeViewEvent[] }>
+
+export type EventsJoinResponse = ApiResponse<undefined>
+export type EventsLeaveResponse = ApiResponse<undefined>
+export type EventsKickResponse = ApiResponse<undefined>
