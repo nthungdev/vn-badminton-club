14f44df359cca5d21c4917ba3a464d3c8776af02
Hung Nguyen <nthungdev@gmail.com>
2024-10-20 04:47:19 -0400

    max toast width

--- components/ToastsOverlay.tsx
+++ components/ToastsOverlay.tsx
@@ -1,23 +1,10 @@
 'use client'
 
-import { ToastOptions, useToastsContext } from '@/app/contexts/ToastsContext'
 import { Toast } from 'flowbite-react'
 import { HiExclamation } from 'react-icons/hi'
-
-function makeToast(options: ToastOptions) {
-  return (
-    <Toast duration={300}>
-      <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-orange-100 text-orange-500 dark:bg-orange-700 dark:text-orange-200">
-        <HiExclamation className="h-5 w-5" />
-      </div>
-      <div className="ml-3 text-sm font-normal">{options.message}</div>
-      <Toast.Toggle />
-    </Toast>
-  )
-}
+import { useToastsContext } from '@/app/contexts/ToastsContext'
 
 export default function ToastsOverlay() {
-  // const { toasts } = useToasts()
   const { toasts } = useToastsContext()
 
   console.log('toasts', toasts)
@@ -25,10 +12,16 @@ export default function ToastsOverlay() {
   return (
     <div
       id="toasts-overlay"
-      className="absolute max-h-full right-0 top-0 p-4 space-y-2 overflow-y-auto"
+      className="absolute max-h-full max-w-sm right-0 top-0 p-4 space-y-2 overflow-y-auto"
     >
       {toasts.map((toast, index) => (
-        <div key={index}>{makeToast(toast)}</div>
+        <Toast key={index} duration={300}>
+          <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-orange-100 text-orange-500 dark:bg-orange-700 dark:text-orange-200">
+            <HiExclamation className="h-5 w-5" />
+          </div>
+          <div className="ml-3 text-sm font-normal">{toast.message}</div>
+          <Toast.Toggle />
+        </Toast>
       ))}
     </div>
   )
