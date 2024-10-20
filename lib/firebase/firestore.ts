b826cfd095c9fc87bae667514be2a81eddf401c9
Hung Nguyen <nthungdev@gmail.com>
2024-10-20 04:44:03 -0400

    update event leave restriction

--- lib/firebase/firestore.ts
+++ lib/firebase/firestore.ts
@@ -13,8 +13,11 @@ import { COLLECTION_EVENTS } from './firestore.constant'
 import { firestore } from './serverApp'
 import { getUserById } from '../authUtils'
 import { EventsCacheKey, getNodeCache } from '../cache'
+import AppError from '../AppError'
 
 const cache = getNodeCache('eventsCache')
+
+const EVENT_CUTOFF = 8 * 60 * 60 * 1000 // 8 hours
 
 function snapshotToEvents(
   snapshot: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>
@@ -172,13 +175,30 @@ async function deleteEvent(eventId: string) {
 }
 
 async function joinEvent(uid: string, eventId: string) {
+  const eventRef = firestore.collection(COLLECTION_EVENTS).doc(eventId)
+
   try {
-    await firestore
-      .collection(COLLECTION_EVENTS)
-      .doc(eventId)
-      .update({
+    const error = await firestore.runTransaction(async (transaction) => {
+      const doc = await transaction.get(eventRef)
+      if (!doc.exists) {
+        return 'Event not found'
+      }
+
+      const data = doc.data() as FirestoreEvent
+      if (data.participantIds.length >= data.slots) {
+        return 'Event is full'
+      }
+
+      transaction.update(eventRef, {
         participantIds: FieldValue.arrayUnion(uid),
       })
+    })
+
+    if (error) {
+      throw new Error(error)
+    }
+
+    console.info(`User ${uid} joined event ${eventId}`)
     cache.del(EventsCacheKey.NewEvents)
   } catch (error) {
     console.error('Error joining event:', error)
@@ -187,15 +207,33 @@ async function joinEvent(uid: string, eventId: string) {
 }
 
 async function leaveEvent(uid: string, eventId: string) {
+  const eventRef = firestore.collection(COLLECTION_EVENTS).doc(eventId)
+
   try {
-    const result = await firestore
-      .collection(COLLECTION_EVENTS)
-      .doc(eventId)
-      .update({
+    const error = await firestore.runTransaction(async (transaction) => {
+      const doc = await transaction.get(eventRef)
+      if (!doc.exists) {
+        return 'Event not found'
+      }
+
+      const data = doc.data() as FirestoreEvent
+
+      if (
+        new Date(data.startTimestamp.seconds * 1000 - EVENT_CUTOFF) < new Date()
+      ) {
+        return 'Cannot leave event at this time'
+      }
+
+      transaction.update(eventRef, {
         participantIds: FieldValue.arrayRemove(uid),
       })
+    })
+
+    if (error) {
+      throw new AppError(error)
+    }
+
     cache.del(EventsCacheKey.NewEvents)
-    return result.isEqual
   } catch (error) {
     console.error('Error leaving event:', error)
     throw new Error('Error leaving event')
