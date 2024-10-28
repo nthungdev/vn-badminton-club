220b99788c7d68f61266dd2c6eef1b7f5a46289c
Hung Nguyen <nthungdev@gmail.com>
2024-10-27 20:49:42 -0400

    refactor kickGuest

--- src/app/api/events/[id]/kickGuest/route.ts
+++ src/app/api/events/[id]/kickGuest/route.ts
@@ -1,9 +1,15 @@
 import { createErrorResponse } from '@/lib/apiResponse'
-import { getEventById, kickGuest } from '@/firebase/firestore'
+import { getEventById } from '@/firebase/firestore'
 import { verifySession } from '@/lib/session'
 import { NextRequest } from 'next/server'
-import { EVENT_NOT_FOUND_ERROR } from '@/constants/errorMessages'
-import { Role } from '@/firebase/definitions'
+import {
+  EVENT_GUEST_NOT_FOUND_ERROR,
+  EVENT_NOT_FOUND_ERROR,
+  EVENT_STARTED_ERROR,
+  UNKNOWN_ERROR,
+} from '@/constants/errorMessages'
+import { kickGuest } from '@/lib/events'
+import AppError from '@/lib/AppError'
 
 interface EventGuestKickRequest {
   guestId: string
@@ -31,13 +37,30 @@ export async function PATCH(
       return createErrorResponse(EVENT_NOT_FOUND_ERROR, 404)
     }
 
-    const isMod = decodedIdToken.role === Role.Mod
-
-    await kickGuest(eventId, data.guestId, decodedIdToken.uid, {
-      checkPermission: !isMod
-    })
+    await kickGuest(
+      eventId,
+      data.guestId,
+      decodedIdToken.uid,
+      decodedIdToken.role
+    )
     return Response.json({ success: true })
   } catch (error) {
+    if (error instanceof AppError) {
+      let statusCode = 400
+      switch (error.message) {
+        case EVENT_GUEST_NOT_FOUND_ERROR:
+        case EVENT_NOT_FOUND_ERROR:
+          statusCode = 404
+          break
+        case EVENT_STARTED_ERROR:
+          statusCode = 403
+          break
+        case UNKNOWN_ERROR:
+        default:
+          statusCode = 400
+      }
+      return createErrorResponse(error.message, statusCode)
+    }
     return createErrorResponse(error, 500)
   }
 }
--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,6 +1,7 @@
 'server-only'
 
 import {
+  EVENT_GUEST_NOT_FOUND_ERROR,
   EVENT_NOT_FOUND_ERROR,
   EVENT_STARTED_ERROR,
   UNAUTHORIZED_ERROR,
@@ -10,7 +11,7 @@ import { Role } from '@/firebase/definitions'
 import { EditEventParams } from '@/firebase/definitions/event'
 import { COLLECTION_EVENTS } from '@/firebase/firestore.constant'
 import { firestore } from '@/firebase/serverApp'
-import { eventReadConverter } from '@/firebase/utils'
+import { eventReadConverter, eventWriteConverter } from '@/firebase/utils'
 import { isPast } from './utils/events'
 import AppError from './AppError'
 
@@ -75,6 +76,77 @@ export async function editEvent(
       return new AppError(errorMessage)
     }
   } catch (error) {
+    return new AppError(UNKNOWN_ERROR, error)
+  }
+}
+
+/**
+ * @throws {AppError} with message either
+ * - UNAUTHORIZED_ERROR
+ * - EVENT_NOT_FOUND_ERROR
+ * - EVENT_GUEST_NOT_FOUND_ERROR
+ * - EVENT_STARTED_ERROR
+ * - UNKNOWN_ERROR
+ */
+export async function kickGuest(
+  eventId: string,
+  guestId: string,
+  uid: string,
+  role: Role
+) {
+  const eventRef = eventCollection
+    .withConverter(eventWriteConverter)
+    .doc(eventId)
+
+  try {
+    const errorMessage = await firestore.runTransaction(async (transaction) => {
+      const doc = await transaction.get(eventRef)
+      const event = doc.data()
+      if (!doc.exists || !event) {
+        return EVENT_NOT_FOUND_ERROR
+      }
+
+      const guest = event.guests.find((guest) => guest.guestId === guestId)
+      if (!guest) {
+        // Might happen when rare race condition: the guest was already kicked by someone else
+        return EVENT_GUEST_NOT_FOUND_ERROR
+      }
+
+      switch (role) {
+        case Role.Mod:
+          break
+        case Role.Member:
+          const hasStarted = isPast(event.startTimestamp)
+          if (hasStarted) {
+            return EVENT_STARTED_ERROR
+          }
+
+          if (event.createdBy === uid) {
+            break
+          } else {
+            // make sure the guest is added by the user
+            if (guest.addedBy === uid) {
+              break
+            }
+          }
+        default:
+          return UNAUTHORIZED_ERROR
+      }
+
+      transaction.update(eventRef, {
+        guests: event.guests.filter((guest) => guest.guestId !== guestId),
+      })
+    })
+
+    if (errorMessage !== undefined) {
+      throw new AppError(errorMessage)
+    }
+
+    console.info(`Guest ${guestId} was kicked from event ${eventId}`)
+  } catch (error) {
+    if (error instanceof AppError) {
+      throw error
+    }
     return new AppError(UNKNOWN_ERROR, error)
   }
 }
