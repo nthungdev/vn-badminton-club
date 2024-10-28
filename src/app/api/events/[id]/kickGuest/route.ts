ee746c4090db5a70d6ab16d5e5619e37a8054bfa
Hung Nguyen <nthungdev@gmail.com>
2024-10-27 21:06:50 -0400

    update to use error message constant

--- src/app/api/events/[id]/kickGuest/route.ts
+++ src/app/api/events/[id]/kickGuest/route.ts
@@ -1,4 +1,4 @@
-import { createErrorResponse } from '@/lib/apiResponse'
+import { createErrorResponse, createSuccessResponse } from '@/lib/apiResponse'
 import { getEventById } from '@/firebase/firestore'
 import { verifySession } from '@/lib/session'
 import { NextRequest } from 'next/server'
@@ -6,6 +6,7 @@ import {
   EVENT_GUEST_NOT_FOUND_ERROR,
   EVENT_NOT_FOUND_ERROR,
   EVENT_STARTED_ERROR,
+  UNAUTHORIZED_ERROR,
   UNKNOWN_ERROR,
 } from '@/constants/errorMessages'
 import { kickGuest } from '@/lib/events'
@@ -21,14 +22,14 @@ export async function PATCH(
 ) {
   const { decodedIdToken } = await verifySession()
   if (!decodedIdToken) {
-    return createErrorResponse('Unauthorized', 401)
+    return createErrorResponse(UNAUTHORIZED_ERROR, 401)
   }
 
   const { id: eventId } = params
   const data: EventGuestKickRequest = await request.json()
 
   if (!data.guestId) {
-    return createErrorResponse('Missing guestId', 400)
+    return createErrorResponse('Missing guestId.', 400)
   }
 
   try {
@@ -43,7 +44,7 @@ export async function PATCH(
       decodedIdToken.uid,
       decodedIdToken.role
     )
-    return Response.json({ success: true })
+    return createSuccessResponse()
   } catch (error) {
     if (error instanceof AppError) {
       let statusCode = 400
