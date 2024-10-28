279237b511696e29b50d0e2991a3bad18addfd6c
Hung Nguyen <nthungdev@gmail.com>
2024-10-28 18:13:22 -0400

    optimize middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,19 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { VerifySessionResult } from './lib/session'
-import { AuthGuard, menu } from './lib/menu'
+import { AuthGuard, menu, menuHref } from './lib/menu'
 
 export async function middleware(request: NextRequest) {
+  const menuItem = menu.find((item) => item.href === request.nextUrl.pathname)
+  if (!menuItem) {
+    // Invalid route, let NextJS route to 404
+    return NextResponse.next()
+  }
+
+  if (menuItem.guard === AuthGuard.Public) {
+    return NextResponse.next()
+  }
+
   const session = request.cookies.get('session')
 
   // Call the authentication endpoint
@@ -16,34 +26,19 @@ export async function middleware(request: NextRequest) {
 
   const { isAuth }: VerifySessionResult = await responseAPI.json()
 
-  const menuItem = menu.find((item) => item.href === request.nextUrl.pathname)
-  if (!menuItem) {
-    // Invalid route, let NextJS route to 404
-    return NextResponse.next()
-  }
-
   switch (menuItem.guard) {
     case AuthGuard.AuthenticatedRequired:
       if (!isAuth) {
-        return NextResponse.redirect(new URL('/signIn', request.url))
+        return NextResponse.redirect(new URL(menuHref.signIn, request.url))
       }
       break
     case AuthGuard.UnauthenticatedRequired:
       if (isAuth) {
-        return NextResponse.redirect(new URL('/', request.url))
+        return NextResponse.redirect(new URL(menuHref.home, request.url))
       }
       break
-    case AuthGuard.Public:
-      return NextResponse.next()
-    default:
-      return NextResponse.next()
   }
 }
-
-// Add your protected routes
-// export const config = {
-//   matcher: ['/:path*'],
-// }
 
 // Routes Middleware should not run on
 export const config = {
