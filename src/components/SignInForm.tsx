953ca48e411b8eda51115c27ada32eb57c889d12
Hung Nguyen <nthungdev@gmail.com>
2024-11-19 04:56:40 -0500

    update ring color

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -98,7 +98,7 @@ function SubmitButton() {
   return (
     <button
       type="submit"
-      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-primary text-white hover:bg-primary-700 focus:outline-none focus:bg-primary-700 disabled:opacity-50 disabled:pointer-events-none"
+      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-primary text-white hover:bg-primary-700 focus:outline-none focus:bg-primary-700 focus:ring-primary-400 disabled:opacity-50 disabled:pointer-events-none"
       disabled={pending}
     >
       Sign In
